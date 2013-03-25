6bacc40064cede20a0d90eda0facb0db101b418b
Olivier Cortès <olive@licorn.org>
2013-03-25 22:05:35 +0100

    Update `install` to work with the new `remote_configuration` feature, which, in case of `install`, is local instead of remote.

--- 1nstall.py
+++ 1nstall.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    This script is meant to bootstrap a brand new machine when I can't
+    bootstrap it from another one, or when it will be used as one of my
+    *principal* machines, aka the machine from which I deploy others.
 
+"""
 import sys
 import os
 
@@ -9,34 +15,35 @@ if __package__ is None:
 
 from sparks import fabric as sf
 
-if sf.lsb_release:
-    if sf.is_vm:
-        if not os.path.exists(os.path.expanduser('~/Dropbox')):
-            if sf.is_parallel:
-                sf._run('ln -sf /media/psf/Home/Dropbox ~/')
 
-        # TODO: implement for vmware.
+@sf.with_remote_configuration
+def main(remote_configuration=None):
+    if remote_configuration.lsb_release:
+        if remote_configuration.is_vm:
+            if not os.path.exists(os.path.expanduser('~/Dropbox')):
+                if remote_configuration.is_parallel:
+                    sf.nofabric._run('ln -sf /media/psf/Home/Dropbox ~/')
+
+                else:
+                    # TODO: implement for vmware.
+                    pass
+
+        for filename in ('bashrc', 'ssh'):
+            sf.nofabric._run('ln -sf %s ~/.%s' % (sf.dotfiles('dot.%s'
+                             % filename), filename))
+
+        sf.nofabric._sudo('apt-get update')
+        sf.nofabric._sudo('apt-get install -y --force-yes gdebi python-pip ssh '
+                          'python-all-dev build-essential')
+        sf.nofabric._sudo('pip install fabric')
 
     else:
-        if os.path.exists('/usr/bin/dropbox'):
-            # OK, just continue.
-            pass
+        # TODO: there's work to do here: install Xcode & CLI tools for Xcode.
+        sf.nofabric._sudo('ruby -e "$(curl -fsSL '
+                          'https://raw.github.com/mxcl/homebrew/go)"')
+        sf.nofabric._sudo('brew update; brew install python pip')
 
-        else:
-            # TODO: install dropbox automatically.
-            print '>> please install dropbox, set it up, and relaunch.'
-            sys.exit(1)
+    os.system('cd ~/Dropbox/configuration; fab -H localhost myenv')
 
-    for filename in ('bashrc', 'ssh'):
-        sf._run('ln -sf %s ~/.%s' % (sf.dotfiles('dot.%s' % filename),
-                filename))
-
-    sf._sudo('apt-get update')
-    sf._sudo('apt-get install -y --force-yes gdebi python-pip ssh '
-             'python-all-dev build-essential')
-    sf._sudo('pip install fabric')
-
-else:
-    sf._sudo('ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"')
-
-os.system('cd ~/Dropbox/configuration; fab -H localhost myenv')
+if __name__ == '__main__':
+    main()
