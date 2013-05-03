973e48263fe08d8b5194a22f247c1c5d90ad8d93
Olivier Cortès <olive@licorn.org>
2013-05-03 22:05:49 +0200

    Ability to return only letters for unique_hash(). It's probably less unique, but this works and we don't need a math-perfect implementation.

--- sparks/foundations/utils.py
+++ sparks/foundations/utils.py
@@ -3,5 +3,8 @@
 import uuid
 
 
-def unique_hash():
+def unique_hash(only_letters=False):
+    if only_letters:
+        return ''.join((chr(int(x) + 97) if x.isdigit() else x)
+                       for x in uuid.uuid4().hex)
     return uuid.uuid4().hex
