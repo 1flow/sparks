a22cc4ac146670d7baa8657c9934553008ce35a9
Olivier Cortès <olive@licorn.org>
2013-05-04 10:52:21 +0200

    bump version in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sparks",
-    version='1.13.7',
+    version='1.13.8',
     author="Olivier Cortès",
     author_email="contact@oliviercortes.com",
     description="My project/machine bootstrap library",
