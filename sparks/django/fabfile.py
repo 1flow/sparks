9e411f49cb7eb63ee67f395523828aab642f2a5d
Olivier Cortès <olive@licorn.org>
2013-05-28 16:20:33 +0200

    Ignore django settings errors on first deployment.

--- sparks/django/fabfile.py
+++ sparks/django/fabfile.py
@@ -308,9 +308,16 @@ def git_pull():
 @with_remote_configuration
 def push_translations(remote_configuration=None):
 
-    if not remote_configuration.django_settings.DEBUG:
-        # remote translations are fetched only on development / test
-        # environments. Production are not meant to host i18n work.
+    try:
+        if not remote_configuration.django_settings.DEBUG:
+            # remote translations are fetched only on development / test
+            # environments. Production are not meant to host i18n work.
+            return
+
+    except AttributeError:
+        LOGGER.warning('push_translations() ignored, remote Django settings '
+                       'cannot be loaded (you can ignore this warning during '
+                       'first deployment.')
         return
 
     with cd(env.root):
