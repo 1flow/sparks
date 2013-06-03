0e6ec2bad64fb5dbdd535a860ed6c5c4a44cd8af
Olivier Cortès <olive@licorn.org>
2013-06-03 21:42:51 +0200

    Switch to our own-flavored fabric, now that sparks depends on the `env.host_string.role` feature, which is not in Fabric main tree.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     url="https://github.com/Karmak23/sparks",
 
     packages=find_packages(),
-    install_requires=['Fabric'],
+    install_requires=['-e git+https://github.com/Karmak23/fabric#egg=fabric'],
 
     keywords=(
         'installation',
