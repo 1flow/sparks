44316f79c05fcc549349f7271b1e6f4369a30ed1
Olivier Cortès <olive@licorn.org>
2013-06-03 22:05:31 +0200

    Ho boy, install_requires… Can't I specify a development version/tag??

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     url="https://github.com/Karmak23/sparks",
 
     packages=find_packages(),
-    install_requires=['-e git+https://github.com/Karmak23/fabric#egg=fabric'],
+    install_requires=['-e git+https://github.com/Karmak23/fabric.git@7bd2eee26d7255c7b0edac0b87c1ee196086668f#egg=Fabric-dev'],
 
     keywords=(
         'installation',
