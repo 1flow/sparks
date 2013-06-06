9c0aa6e1ce181a872dd56a88e7cbdef7872f99d2
Olivier Cortès <olive@licorn.org>
2013-06-07 00:13:52 +0200

    Update description & keywords.

--- setup.py
+++ setup.py
@@ -8,15 +8,16 @@ setup(
     version='2.5.2',
     author="Olivier Cortès",
     author_email="contact@oliviercortes.com",
-    description="My project/machine bootstrap library",
+    description="My Django project & cloud deployment library (Fabric based)",
     url="https://github.com/Karmak23/sparks",
-
     packages=find_packages(),
     install_requires=['Fabric'],
-
     keywords=(
         'installation',
         'management',
+        'django',
+        'cloud',
+        'deployment',
         'bootstraping',
     ),
     license='New BSD',
