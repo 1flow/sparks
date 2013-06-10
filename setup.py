baf8f336a46dcdb1f6264937b51240cb111b5d87
Olivier Cortès <olive@licorn.org>
2013-06-10 11:00:40 +0200

    bump version in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sparks",
-    version='2.6.5',
+    version='2.6.6',
     author="Olivier Cortès",
     author_email="contact@oliviercortes.com",
     description="My Django project & cloud deployment library (Fabric based)",
