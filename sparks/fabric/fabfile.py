bce4f3d65ff0fd38ab6320d24b0896d0e62c9745
Olivier Cortès <oc@1flow.io>
2014-02-22 14:48:36 +0100

    Mini-changes to docker docstring.

--- sparks/fabric/fabfile.py
+++ sparks/fabric/fabfile.py
@@ -972,13 +972,14 @@ def docker(remote_configuration=None):
     warning("This functions is not yet implemented.")
 
     """
-    http://docs.docker.io/en/latest/installation/ubuntulinux/#ubuntu-precise
+    # http://docs.docker.io/en/latest/installation/ubuntulinux/#ubuntu-precise
+
     sudo apt-get update
     sudo apt-get install linux-image-generic-lts-raring linux-headers-generic-lts-raring
     sudo reboot
+
     sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
-    sudo sh -c "echo deb http://get.docker.io/ubuntu docker main\
-    > /etc/apt/sources.list.d/docker.list"
+    sudo sh -c "echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
     sudo apt-get update
     sudo apt-get install lxc-docker
 
