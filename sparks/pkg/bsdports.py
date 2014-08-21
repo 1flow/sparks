ca21c1296e3e5849a84ce6efc174472f1b7beaf7
Olivier Cortès <oc@1flow.io>
2014-08-21 12:08:27 +0200

    FreeBSD ports enhancements, notably is_installed() should be now reliable.

--- sparks/pkg/bsdports.py
+++ sparks/pkg/bsdports.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from ..fabric import run, with_remote_configuration
+from ..fabric import sudo, with_remote_configuration
 from ..fabric.utils import list_or_split
 from .common import is_installed, search
 
@@ -11,35 +11,61 @@ from .common import is_installed, search
 @with_remote_configuration
 def ports_usable(remote_configuration=None):
 
-    return remote_configuration.is_freebsd
+    if not remote_configuration.is_freebsd:
+        return False
+
+    if not os.path.exists('/usr/local/bin/sudo'):
+        LOGGER.error(u'sudo is not installed, sparks PKG management will '
+                     u'not work at all. Please install security/sudo before '
+                     u'continuing.')
+        raise SystemExit(1)
+
+    if not os.path.exists('/usr/local/sbin/portmaster'):
+        LOGGER.error(u'portmaster is not installed, sparks PKG management will '
+                     u'not work properly. Please install ports-mgmt/portmaster '
+                     u'before continuing.')
+        raise SystemExit(1)
+
+    # Not needed now that we have --list-origins to check installed packages.
+    # But keeping it here for memories in case we need it. NOTE: portsearch -u
+    # is so long it renders the whole thing unusable…
+    #
+    #if not os.path.exists('/usr/local/bin/portsearch'):
+    #    LOGGER.error(u'portsearch is not installed, sparks PKG management will '
+    #                 u'not work properly. Please install ports-mgmt/portsearch '
+    #                 u'before continuing.')
+    #    raise SystemExit(1)
+
+    return True
 
 
 def ports_is_installed(pkg):
     """ Return ``True`` if a given port is installed. """
 
-    return is_installed('portmaster -l | grep %s >/dev/null 2>&1' % pkg)
+    return is_installed("portmaster -l --list-origins "
+                        "| grep '^%s$' >/dev/null 2>&1" % pkg)
 
 
 def ports_add(pkgs):
     for pkg in list_or_split(pkgs):
         if not ports_is_installed(pkg):
-            run('portmaster %s' % pkg)
+            sudo('portmaster %s' % pkg)
 
 
 def ports_del(pkgs):
     for pkg in list_or_split(pkgs):
         if ports_is_installed(pkg):
-            run('pkg_delete -rf %s' % pkg)
+            sudo('pkg_delete -rf %s' % pkg)
 
 
 def ports_update():
 
-    run('portsnap update')
+    sudo('portsnap update')
 
 
 def ports_upgrade():
 
-    run('portmaster -Da')
+    sudo('portmaster -Da')
 
 
 def ports_search(pkgs):
