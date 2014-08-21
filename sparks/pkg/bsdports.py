137759677b656c05a0ca9a4bce58ff10ccf9a8ab
Olivier Cortès <oc@1flow.io>
2014-08-21 11:15:45 +0200

    update/remove comments in bsdports.

--- sparks/pkg/bsdports.py
+++ sparks/pkg/bsdports.py
@@ -15,15 +15,12 @@ def ports_usable(remote_configuration=None):
 
 
 def ports_is_installed(pkg):
-    """ Return ``True`` if a given port is installed.
-    """
+    """ Return ``True`` if a given port is installed. """
 
     return is_installed('portmaster -l | grep %s >/dev/null 2>&1' % pkg)
 
 
 def ports_add(pkgs):
-    """ Add a local package via ports. """
-
     for pkg in list_or_split(pkgs):
         if not ports_is_installed(pkg):
             run('portmaster %s' % pkg)
@@ -36,13 +33,11 @@ def ports_del(pkgs):
 
 
 def ports_update():
-    """ Update Homebrew formulas. """
 
     run('portsnap update')
 
 
 def ports_upgrade():
-    """ Upgrade outdated brew packages. """
 
     run('portmaster -Da')
 
