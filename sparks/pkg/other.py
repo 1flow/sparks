32a72ad969368863c53a7249d72f801436c813b6
Olivier Cortès <oc@1flow.io>
2014-09-03 18:42:16 +0200

    NPM and GEM get QUIET. gem doesn't use sudo anymore but only run().

--- sparks/pkg/other.py
+++ sparks/pkg/other.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from ..fabric import run, sudo
+from ..fabric import QUIET, run, sudo
 from ..fabric.utils import list_or_split
 from .common import is_installed, search
 
@@ -18,7 +18,7 @@ def npm_is_installed(pkg):
 def npm_add(pkgs):
     for pkg in list_or_split(pkgs):
         if not npm_is_installed(pkg):
-            run('npm install %s' % pkg)
+            run('npm install %s' % pkg, quiet=QUIET)
 
 
 def npm_search(pkgs):
@@ -41,7 +41,7 @@ def gem_is_installed(pkg):
 def gem_add(pkgs):
     for pkg in list_or_split(pkgs):
         if not gem_is_installed(pkg):
-            sudo('gem install %s' % pkg)
+            run('gem install %s' % pkg, quiet=QUIET)
 
 
 def gem_search(pkgs):
