7262df72a16151483af3b91766c0dc881e132354
Olivier Cortès <oc@1flow.io>
2015-05-29 10:28:05 +0200

    usage doc.

--- sparks/django/middleware.py
+++ sparks/django/middleware.py
@@ -8,7 +8,39 @@ _user = local()
 
 class CurrentUserMiddleware(object):
 
-    """ Store current user locally for each request. """
+    """ Store current user locally for each request.
+
+    Usage:
+
+    - in settings:
+
+         MIDDLEWARE_CLASSES = (
+            'django.contrib.sessions.middleware.SessionMiddleware',
+            'django.middleware.common.CommonMiddleware',
+            'django.middleware.csrf.CsrfViewMiddleware',
+            'django.contrib.auth.middleware.AuthenticationMiddleware',
+
+            …
+
+            'sparks.django.middleware.CurrentUserMiddleware',
+
+            …
+        )
+
+    - in models:
+
+        class CreatorAbstractModel(models.Model):
+
+            ''' Common Abstract base to many models. '''
+
+            user = models.ForeignKey('User', null=True, blank=True,
+                                     verbose_name=_(u'creator'),
+                                     default=get_current_user)
+
+            class Meta:
+                abstract = True
+
+    """
 
     def process_request(self, request):
         """ Store request user. """
